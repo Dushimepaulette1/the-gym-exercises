55268a30d19e6e997b0b9742133a2a4125a1ef7e
Dushime Paulette <169043690+Dushimepaulette1@users.noreply.github.com>
2024-11-17 13:35:44 +0200

    my first trial

---
+++ fifth-exercise.js
@@ -0,0 +1,13 @@
+/*Write a function that takes two parameters (a string and a number) and returns the string repeated the given number of times.*/
+//takes two parameters string and a number
+//returns a string reapeted a number of times
+function reapetedString(number, string) {
+  stringCounts = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (typeof [1] === "string") {
+      stringCounts++;
+    }
+  }
+  return stringCounts;
+}
+console.log(reapetedString(4, "paulette"));
