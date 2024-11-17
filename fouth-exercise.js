4d44945c70f97177d75b41fd3c068ba5134d7db5
Dushime Paulette <169043690+Dushimepaulette1@users.noreply.github.com>
2024-11-17 12:25:01 +0200

    checks if a number is even or odd

---
+++ fouth-exercise.js
@@ -0,0 +1,18 @@
+/*Create a function called isEven that takes a number as an argument and returns true if the number is even, and false if it's odd.*/
+
+//takes a number as an argument
+//true if number is even
+// a number is even when if that number % 2 is 0
+//false if number is odd
+// odd when the number % 2 !== 0
+function isEven(number) {
+  if (number === "") {
+    return "Please enter a number";
+  }
+  if (number % 2 === 0) {
+    return true;
+  } else if (number % 2 !== 0) {
+    return false;
+  }
+}
+console.log(isEven(66));
