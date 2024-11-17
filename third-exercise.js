35118852613206d221070ec8a2e9e89c156d5d81
Dushime Paulette <169043690+Dushimepaulette1@users.noreply.github.com>
2024-11-17 12:00:47 +0200

    created a function

---
+++ third-exercise.js
@@ -0,0 +1,6 @@
+//Write a function that counts the number of vowels (a, e, i, o, u) in a given string and returns that count.
+//counting number of vowels in a string 
+//it must return the number of the vowel count 
+function vowelCount(vowels){
+    
+}
