2ea931d7df49d1a87c00d6ebddeac0b2c63e2865
Dushime Paulette <169043690+Dushimepaulette1@users.noreply.github.com>
2024-11-17 12:14:31 +0200

    did the function to count how many vowels are in the string and chnaged the function name

--- third-exercise.js
+++ third-exercise.js
@@ -1,6 +1,20 @@
 //Write a function that counts the number of vowels (a, e, i, o, u) in a given string and returns that count.
-//counting number of vowels in a string 
-//it must return the number of the vowel count 
-function vowelCount(vowels){
-    
+//counting number of vowels in a string
+//it must return the number of the vowel count
+function countVowels(str) {
+  vowelCount = 0;
+  str = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      vowelCount++;
+    }
+  }
+  return vowelCount;
 }
+console.log(countVowels("eeuuooiiEE"));
