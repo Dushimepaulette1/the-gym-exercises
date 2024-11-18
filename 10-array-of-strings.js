e1078e4793580923d1118408d63bef3f7f17ff44
Dushime Paulette <169043690+Dushimepaulette1@users.noreply.github.com>
2024-11-18 12:27:52 +0200

    made changes

--- 10-array-of-strings.js
+++ 10-array-of-strings.js
@@ -1,6 +1,6 @@
 /* Write a function that takes an array of strings and returns a new array with only the strings that have more than 5 characters.*/
 //array of strings
-//returns a new array of strings with only 5 characters
+//returns a new array of strings with characters greater than 5
 function arrayOfStrings(arr) {
   fiveStrings = [];
   for (let i = 0; i < arr.length; i++) {
